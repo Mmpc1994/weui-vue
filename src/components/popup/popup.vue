05005532c51cf072dbf5193d0a192c5b882f561b
maopengcheng <maopengcheng@isa.qa>
2017-08-09 21:34:54 +0800

    popup 组件

---
+++ src/components/popup/popup.vue
@@ -0,0 +1,35 @@
+<template>
+  <transition name="popup">
+    <div class="popup" v-show="visible">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+<script>
+  import Popup from 'vue-popup';
+  import 'vue-popup/lib/popup.css';
+
+  export default {
+    name: 'popup',
+    mixins: [Popup],
+    props: {
+      visible: Boolean
+    }
+  };
+</script>
+<style lang="scss">
+  @import "src/styles/index.scss"; 
+  .popup{
+    overflow: scroll;
+    position: fixed;
+    background: $colorWhite;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    backface-visibility: hidden;
+    transition: 0.2s ease-out;
+  }
+  .popup-enter, .popup-leave-active{
+    transform: translate3d(100%, -50%, 0)
+  }
+</style>
