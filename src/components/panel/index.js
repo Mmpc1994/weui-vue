6497c5d1d8663c5769b5e531e00777ddbc7bbd43
Mmpc1994 <maopengcheng1994@gmail.com>
2017-08-09 21:51:00 +0800

    panel 组件

---
+++ src/components/panel/index.js
@@ -0,0 +1,11 @@
+import panel from './panel.vue'
+import panelBd from './panel_bd.vue'
+import panelFt from './panel_ft.vue'
+import panelHd from './panel_hd.vue'
+
+export {
+  panel,
+  panelBd,
+  panelFt,
+  panelHd
+}
