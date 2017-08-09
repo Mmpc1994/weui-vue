128c6b60895733ee595066b61196fa83756fea61
maopengcheng <maopengcheng@isa.qa>
2017-08-09 21:25:11 +0800

    cell 组件

---
+++ src/components/cells/cell.vue
@@ -0,0 +1,77 @@
+<template>
+  <a class="cell" layout="row" layout-align="center center" @click="go">
+    <div class="cell_hd">
+      <slot name="thumb"></slot>
+    </div>
+    <div class="cell_bd" flex>
+      <p>{{label}}</p>
+    </div>
+    <div class="cell_ft" :class="{'link' : link}">
+      <slot name="value">{{value}}</slot>
+    </div>
+  </a>
+</template>
+
+<script>
+  export default {
+    name: 'cell',
+    props: {
+      value: String,
+      label: String,
+      to: [Object, String],
+      link: {
+        default() {
+          return true;
+        }
+      }
+    },
+    methods: {
+      go() {
+        if (!this.to) {
+          return;
+        }
+        this.$router.push(this.to);
+      }
+    }
+  };
+</script>
+
+<style lang="scss">
+  @import "src/styles/index.scss"; 
+  .cell {
+    padding:  10px 15px;
+    position: relative;
+    &::after {
+      @extend .borderBottom;
+    }
+  }
+  .cell_bd {
+    color: $colorDark;
+  }
+
+  .cell_ft {
+    text-align: right;
+    color: $textColor;
+    padding-right: 13px;
+    position: relative;
+  }
+
+  .link{
+    &::after {
+      content: " ";
+      display: inline-block;
+      height: 6px;
+      width: 6px;
+      border-width: 2px 2px 0 0;
+      border-color: #C8C8CD;
+      border-style: solid;
+      transform: matrix(0.71, 0.71, -0.71, 0.71, 0, 0);
+      position: absolute;
+      top: 50%;
+      margin-top: -4px;
+      right: 2px;
+      z-index:2;
+    }
+  }
+
+</style>
---
+++ src/components/cells/cells.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cells">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'cells'
+  };
+</script>
+<style lang="scss">
+  @import "src/styles/index.scss";
+
+  .cells_title + .cells{
+    margin-top: 0;
+  }
+
+  .cells{
+    background: $colorWhite;
+    padding-top: 0px;
+    margin-top: 15px;
+    overflow: hidden;
+    position: relative;
+    line-height: 1.47;
+
+    &::before{
+      @extend .borderTop;
+      top:-2px;
+    }
+  }
+</style>
