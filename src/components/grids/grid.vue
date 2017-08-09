12f831bef4030eef1e7ad04b4668fa4c120c3bed
maopengcheng <maopengcheng@isa.qa>
2017-08-09 21:23:36 +0800

    grids 组件

---
+++ src/components/grids/grid.vue
@@ -0,0 +1,49 @@
+<template>
+  <a href="javascript:void(0)" class="grid">
+    <div class="grid_value">
+      <slot name="value">{{value}}</slot>
+    </div>
+    <p class="grid_label">{{label}}</p>
+  </a>
+</template>
+<script>
+  export default {
+    name: 'grid',
+    props: {
+      value: String,
+      label: String
+    }
+  };
+</script>
+<style lang="scss">
+  @import "src/styles/index.scss"; 
+  .grid{
+    position: relative;
+    padding: 20px 10px;
+    float: left;
+    width: 33.3333%;
+    box-sizing: border-box;
+    &::before {
+      @extend .borderRight;
+    }
+    &::after {
+      @extend .borderBottom;
+    }
+  }
+  .grid_value {
+    text-align: center;
+    color: $colorRed;
+    font-size: 1.8rem;
+  }
+
+  .grid_label{
+    display: block;
+    text-align: center;
+    color: $colorDark;
+    position: relative;
+    top: 5px;
+    color: $textColor;
+    font-size: 1rem;
+    @include textOverflowCut;
+  }
+</style>
---
+++ src/components/grids/grids.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="grids">
+    <slot></slot>
+  </section>
+</template>
+<style lang="scss">
+  @import "src/styles/index.scss"; 
+  .grids{
+    position: relative;
+    overflow: hidden;
+    background: $colorWhite;
+    &::after{
+      @extend .borderLeft;
+    }
+    &::before{
+      @extend .borderTop;
+    }
+  }
+</style>
