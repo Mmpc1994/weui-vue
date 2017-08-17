eb5f33012f467c0c47251ebd5568c85e6d2f0467
Mmpc1994 <maopengcheng1994@gmail.com>
2017-08-17 11:12:20 +0800

    tab 组件

---
+++ src/components/tabbar/tab.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tab">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tabs'
+  }
+</script>
+
+<style lang="scss">
+  .tab {
+    height: 100%;
+    position: relative;
+  }
+</style>
