f8daadaf5460428423e780932fcb9d89ab8d70f4
Mmpc1994 <maopengcheng1994@gmail.com>
2017-08-17 22:33:43 +0800

    navbar 组件初始化

---
+++ src/components/navbar/src/navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cls-tab">
+    <div class="cls-navbar" layout>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "src/styles/variables";
+
+  .cls-navbar{
+    position: absolute;
+    width: 100%;
+    top: 0;
+    background-color: $darkWhite;
+  }
+</style>
--- src/components/tabbar/src/tabbar.vue
+++ src/components/tabbar/src/tabbar.vue
@@ -22,19 +22,6 @@
 
 <style lang="scss">
   @import 'src/styles/variables.scss';
-  
-  .cls-tab {
-    height: 100%;
-    position: relative;
-  }
-
-  .cls-tab__panel {
-    box-sizing: border-box;
-    height: 100%;
-    padding-bottom: 50px;
-    overflow: hidden;
-    -webkit-overflow-scrolling: touch;
-  }
 
   .cls-tabbar {
     position: absolute;
